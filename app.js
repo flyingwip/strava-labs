5152efecf7bf531baa90197774e80a3f367b0edb
Martijn Wip <flyingwip@gmail.com>
2018-12-26 16:11:59 +0100

    add nodemon

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const port = 3000
 const strava = require('strava-v3');
 
 
+// payin with the config
+app.get('/config', (req, res) => {
+
+	res.send(strava);
+
+});
+
+app.get('/test', (req, res) => {
+
+	res.send('test die sizzle!');
+
+});
+
+
 // login
 app.get('/login', (req, res) => {
 	// make the call to get the redirect url
@@ -15,24 +29,12 @@ app.get('/login', (req, res) => {
 // request with the code
 app.get('/', (req, res) => {
 
-	//const code = req.query.code;
+	//strava.athlete.listActivities({id:11835180, per_page:10, gear_id:"b5377602"},function(err,payload,limits) {
+	strava.athlete.listActivities({id:11835180, gear_id:"b5377602"},function(err,payload,limits) {
+	    //do something with your payload, track rate limits
+	    res.send(payload);
+	});		
 
-	//strava.oauth.getToken(code,function(err,payload,limits) {
-	    
-		// now get activities
-		strava.athlete.listActivities({id:11835180},function(err,payload,limits) {
-		    //do something with your payload, track rate limits
-		    res.send(payload);
-		});		
-
-
-
-
-	    //res.send(payload);
-	//});
-
-
-	//res.send('Hello Strava!' + code);
 })
 
 // strava.athletes.get({id:11835180},function(err,payload,limits) {
@@ -41,6 +43,17 @@ app.get('/', (req, res) => {
 // });
 
 
+
+//strava.oauth.getToken(code,function(err,payload,limits) {
+
+// now get activities
+// strava.athlete.listActivities({id:11835180},function(err,payload,limits) {
+//     //do something with your payload, track rate limits
+//     res.send(payload);
+// });		
+
+//res.send(payload);
+//});
 
 
 
